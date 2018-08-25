0201c6be668d8bea9fde94671ce062669fb58e44
Oleh <oleh.martyniv@gmail.com>
2018-08-25 20:25:53 +0300

    Updated getDriver(): returns instance according to browser type

--- src/main/java/myprojects/automation/assignment3/BaseScript.java
+++ src/main/java/myprojects/automation/assignment3/BaseScript.java
@@ -3,6 +3,8 @@ package myprojects.automation.assignment3;
 import myprojects.automation.assignment3.utils.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.File;
@@ -20,7 +22,16 @@ public abstract class BaseScript {
     public static WebDriver getDriver() {
         String browser = Properties.getBrowser();
         switch (browser) {
-            // TODO prepare required WebDriver instance according to browser type
+            case "firefox":
+                System.setProperty(
+                        "webdriver.gecko.driver",
+                        new File(BaseScript.class.getResource("/geckodriver.exe").getFile()).getPath());
+                return new FirefoxDriver();
+            case "internet explorer":
+                System.setProperty(
+                        "webdriver.ie.driver",
+                        new File(BaseScript.class.getResource("/IEDriverServer.exe").getFile()).getPath());
+                return new InternetExplorerDriver();
             default:
                 System.setProperty(
                         "webdriver.chrome.driver",
@@ -38,7 +49,7 @@ public abstract class BaseScript {
     public static EventFiringWebDriver getConfiguredDriver() {
         WebDriver driver = getDriver();
 
-       // TODO configure browser window (set timeouts, browser pindow position) and connect loggers.
+        // TODO configure browser window (set timeouts, browser pindow position) and connect loggers.
         throw new UnsupportedOperationException("Method doesn't return configured WebDriver instance");
     }
 }
